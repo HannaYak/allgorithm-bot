5e51340ba4e1bf3574b3cd932be5b4fed9ab9ca4
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-17 13:04:12 +0100

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -20,7 +20,7 @@ async def profile_with_card(message: types.Message):
     loyalty = user.get('loyalty', 0)
     
     # Определяем какую картинку отправлять (от 0 до 5)
-    image_number = min(games_played, 5)  # больше 5 — всё равно 5_games.jpg
+    image_number = min(games_played, 10)  # больше 5 — всё равно 5_games.jpg
     image_path = os.path.join(LOYALTY_DIR, f"{image_number}_games.jpg")
     
     # Если файла нет — отправим запасной вариант
