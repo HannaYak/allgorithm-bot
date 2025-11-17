82471f563aa2bb011c318de936318e1549bbbb9e
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-17 11:46:01 +0100

    Refactor imports and update __all__ in __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,15 @@
-from . import start, games, profile, admin, help
+from .start import router as start_router
+from .games import router as games_router
+from .profile import router as profile_router
+from .help import router as help_router
+from .payments import router as payments_router
+from .admin import router as admin_router
 
-__all__ = ["start", "games", "profile", "admin", "help"]
+start = start_router
+games = games_router
+profile = profile_router
+help = help_router
+payments = payments_router
+admin = admin_router
+
+__all__ = ["start", "games", "profile", "help", "payments", "admin"]
