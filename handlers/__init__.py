4d337f7e95b8a910a756b632c52e2a0642b39145
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-20 14:42:09 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,18 +2,16 @@
 from .start import router as start_router
 from .games import router as games_router
 from .profile import router as profile_router
-from .help import router as help_router
 from .payments import router as payments_router
 from .admin import router as admin_router
-from .support import router as support_router
+from .common import router as common_router
 
 # Теперь всё доступно как start_router, games_router и т.д.
 __all__ = [
     "start_router",
     "games_router",
     "profile_router",
-    "help_router",
     "payments_router",
     "admin_router",
-    "support_router"
+    "common_router"
 ]
