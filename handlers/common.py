e639baac63c40964420f80598f0f2fc6553453a8
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-22 15:00:02 +0100

    Update common.py

--- handlers/common.py
+++ handlers/common.py
@@ -1,5 +1,5 @@
 # handlers/common.py — Правила + Помощь + Ответ админа (100% работает)
-from aiogram import Router, F
+from aiogram import Router, F, types
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
