d30003f79f1156194036403b8d34fb0a5f94d4df
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-27 16:35:45 +0100

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,1 +1,9 @@
+from aiogram import Router, F, types
+from config import ADMIN_ID
 
+router = Router()
+
+@router.message(F.from_user.id == ADMIN_ID)
+async def admin_secret(message: types.Message):
+    if message.text == "/admin":
+        await message.answer("Админка в разработке. Скоро будет огонь 🔥")
