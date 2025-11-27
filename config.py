328df5322f53d821f0cbfb2471da57e3afe54338
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-27 16:44:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from os import getenv
 
-TOKEN = getenv("BOT_TOKEN")
+TOKEN = getenv("7801157571:AAE9aS4ZY0A_VLf4Dkt2RULAaqFLwxykWj8")
 ADMIN_ID = 5456905649  # ← ТВОЙ ID
-WEBHOOK_URL = getenv("WEBHOOK_URL")  # https://chic-wisdom-bot.up.railway.app
+WEBHOOK_URL = getenv("https://chic-wisdom-bot.up.railway.app")  # https://chic-wisdom-bot.up.railway.app
 
 bot = Bot(
     token=TOKEN,
