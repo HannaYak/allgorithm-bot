51bf497cf88ca7d5ee2ddda4d0f661af7d05ccf9
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-30 19:07:20 +0100

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,17 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
+def main_menu():
+    kb = [
+        [KeyboardButton(text="Игры"), KeyboardButton(text="Мероприятия")],
+        [KeyboardButton(text="Личный кабинет"), KeyboardButton(text="Помощь")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
+def games_menu():
+    kb = [
+        [KeyboardButton(text="Talk & Toast")],
+        [KeyboardButton(text="Stock & Know")],
+        [KeyboardButton(text="Быстрые свидания")],
+        [KeyboardButton(text="Назад в меню")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
