0a72a4b8cb9891a82bb82750147632ddf8fc1d57
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-30 19:37:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from config import BOT_TOKEN
 from database import init
+
 from handlers.start import router as start_router
 from handlers.registration import router as reg_router
 from handlers.events import router as events_router
 from handlers.admin import router as admin_router
 
-bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+# НОВЫЙ СПОСОБ ДЛЯ aiogram 3.13+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode="HTML")
+)
 dp = Dispatcher()
 
 async def main():
@@ -17,7 +23,7 @@ async def main():
     dp.include_router(events_router)
     dp.include_router(admin_router)
     
-    print("Бот запущен!")
+    print("Бот запущен и работает 24/7!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
