e67bdcb0c1e211a46a2955bf65f7ccc67f3126e1
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2025-11-30 19:10:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init
 from handlers.start import router as start_router
-# сюда потом добавим остальные роутеры
+from handlers.registration import router as reg_router
+from handlers.events import router as events_router
+from handlers.admin import router as admin_router
 
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
@@ -11,11 +13,11 @@ dp = Dispatcher()
 async def main():
     await init()
     dp.include_router(start_router)
-    # dp.include_router(registration_router)
-    # dp.include_router(events_router)
-    # dp.include_router(admin_router)
+    dp.include_router(reg_router)
+    dp.include_router(events_router)
+    dp.include_router(admin_router)
     
-    print("Бот запущен и работает 24/7")
+    print("Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
